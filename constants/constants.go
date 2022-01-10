06c1e54771f8c285e71fb04d9e914efa7de5b3f1
sankethkini <kinisanketh@gmail.com>
2022-01-10 19:45:01 +0530

    added required constants

---
+++ constants/constants.go
@@ -0,0 +1,32 @@
+package constants
+
+import "github.com/sankethkini/StudentData/domain/course"
+
+var AllCourses = []course.Course{
+	{
+		Name: "A",
+		Code: "A",
+	},
+	{
+		Name: "B",
+		Code: "B",
+	},
+	{
+		Name: "C",
+		Code: "C",
+	},
+	{
+		Name: "D",
+		Code: "D",
+	},
+	{
+		Name: "E",
+		Code: "E",
+	},
+	{
+		Name: "F",
+		Code: "F",
+	},
+}
+
+const NoOfCourses int = 4
