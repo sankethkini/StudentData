a5ca7498b68deb9cbb3545cf5397e1f5cf69d683
sankethkini <kinisanketh@gmail.com>
2022-01-12 12:58:04 +0530

    modified application file added more functionality
    added display,add, delete and save functionality
    also tested with appropriate test cases

--- application/application.go
+++ application/application.go
@@ -1,23 +1,80 @@
 package application
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/sankethkini/StudentData/adapters"
+	"github.com/sankethkini/StudentData/domain/course"
+	"github.com/sankethkini/StudentData/domain/user"
+)
+
 //alisaing map
 type data map[string]interface{}
 
-//validators of input
-func InputValidator(userdata data) error {
+//init function called one time
+func init() {
 
+	//intializing file adapter
+	fileAdapter, err := adapters.Init(adapters.FILE)
+	if err != nil {
+		fmt.Println(err)
+		Exit(nil)
+	}
+
+	//intializing memory adapter
+	memoryAdapter, err := adapters.Init(adapters.MEMORY)
+	if err != nil {
+		fmt.Println(err)
+		Exit(nil)
+	}
+
+	//retriving all data from disk
+	diskdata, err := fileAdapter.RetriveAll("", 1)
+	if err != nil {
+		fmt.Println(err)
+		Exit(nil)
+	}
+
+	//adding into memory
+	err = memoryAdapter.Save(diskdata)
+	if err != nil {
+		fmt.Println(err)
+		Exit(nil)
+	}
+}
+
+//function to return message
+func createMessage(m string) []data {
+	var msg []data
+	mp := make(map[string]interface{})
+	mp["message"] = m
+	msg = append(msg, mp)
+	return msg
+}
+
+//validators of input
+func inputValidator(userdata data) error {
+
+	//checking name
 	curname := userdata["fname"].(string)
 	if len(curname) < 2 {
 		return NoNameErr
 	}
+
+	//checking rollnumber
 	curroll := userdata["rollnum"].(string)
 	if len(curroll) < 2 {
 		return NoRollNum
 	}
+
+	//checking address
 	curadd := userdata["address"].(string)
 	if len(curadd) < 2 {
 		return NoAddress
 	}
+
+	//checking age
 	curage := userdata["age"].(int)
 	if curage <= 0 || curage >= 120 {
 		return AgeErr
@@ -26,24 +83,133 @@ func InputValidator(userdata data) error {
 	return nil
 }
 
+//validations for rollnumber exists
+func checkForRoll(rollnum string) error {
+	//intializing memory adapter
+	memoryAdapter, err := adapters.Init(adapters.MEMORY)
+	if err != nil {
+		return err
+	}
+
+	//checking existence of simialr rollnum
+	isExists := memoryAdapter.Retrive("rollnum", rollnum)
+	if isExists {
+		return RollExists
+	}
+	return nil
+}
+
 //function to add user
-func Add(data) ([]data, error) {
-	return nil, nil
+func Add(userdata data) ([]data, error) {
+
+	//checking validity of user input
+	validationerr := inputValidator(userdata)
+	if validationerr != nil {
+		return nil, validationerr
+	}
+
+	//checking for rollnum existence
+	rollnum := userdata["rollnum"].(string)
+	isRollexists := checkForRoll(rollnum)
+	if isRollexists != nil {
+		return nil, isRollexists
+	}
+
+	//saving in memory
+	memoryAdapter, err := adapters.Init(adapters.MEMORY)
+	if err != nil {
+		return nil, err
+	}
+
+	//adding data into user struct
+	curuser := user.User{}
+	curuser.Fname = userdata["fname"].(string)
+	curuser.RollNo = userdata["rollnum"].(string)
+	curuser.Age = userdata["age"].(int)
+	curuser.Adress = userdata["address"].(string)
+	curuser.Courses = userdata["courses"].([]course.Course)
+
+	//adding user
+	err = memoryAdapter.Save(curuser)
+	if err != nil {
+		return nil, err
+	}
+
+	//message
+	msg := createMessage("user added successfuly")
+	return msg, err
 }
 
 //function to display
-func Display(data) ([]data, error) {
-	return nil, nil
+func Display(userdata data) ([]user.User, error) {
+	//intializing memory adapter
+	memoryAdapter, err := adapters.Init(adapters.MEMORY)
+	if err != nil {
+		return nil, err
+	}
+
+	field := userdata["field"].(string)
+	order := userdata["order"].(int)
+
+	//retriving all data
+	items, err := memoryAdapter.RetriveAll(field, order)
+	if err != nil {
+		return nil, err
+	}
+	allitems := items.([]user.User)
+	return allitems, nil
 }
 
-func Delete(data) ([]data, error) {
-	return nil, nil
+func Delete(userdata data) ([]data, error) {
+	roll := userdata["rollnum"].(string)
+
+	//intializing memory adapter
+	memoryAdapter, err := adapters.Init(adapters.MEMORY)
+	if err != nil {
+		return nil, err
+	}
+
+	//deleting user
+	err = memoryAdapter.Delete("rollnum", roll)
+	if err != nil {
+		return nil, err
+	}
+
+	msg := createMessage("user deleted successfuly")
+	return msg, err
 }
 
 func Save(data) ([]data, error) {
-	return nil, nil
+	//intializing file adapter
+	fileadapter, err := adapters.Init(adapters.FILE)
+	if err != nil {
+		return nil, err
+	}
+
+	//intializing memory adapter
+	memoryadapter, err := adapters.Init(adapters.MEMORY)
+	if err != nil {
+		return nil, err
+	}
+
+	//retirving all by name
+	alldata, err := memoryadapter.RetriveAll("name", 1)
+	if err != nil {
+		return nil, err
+	}
+
+	//saving on disk
+	err = fileadapter.Save(alldata)
+	if err != nil {
+		return nil, err
+	}
+	msg := createMessage("users saved to disk successfuly")
+	return msg, err
+
 }
 
+//exit function
 func Exit(data) ([]data, error) {
+	os.Exit(1)
 	return nil, nil
 }
--- application/application_test.go
+++ application/application_test.go
@@ -1,7 +1,12 @@
 package application
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/sankethkini/StudentData/adapters"
+	"github.com/sankethkini/StudentData/constants"
+	"github.com/sankethkini/StudentData/domain/user"
 )
 
 var testForValidator = []struct {
@@ -11,23 +16,164 @@ var testForValidator = []struct {
 	age      int
 	expected error
 }{
-	{fname: "sanket", rollnum: "t77", adress: "abcd, defgh, jiop", age: 18, expected: nil},
+	{fname: "sanket", rollnum: "t77", adress: "abcd, defgh, jiop", age: 17, expected: nil},
 	{fname: "", rollnum: "t77", adress: "abcd, defgh, jiop", age: 18, expected: NoNameErr},
 	{fname: "sanket", rollnum: "", adress: "", age: 18, expected: NoRollNum},
 	{fname: "sanket", rollnum: "t77", adress: "abcd, defgh, jiop", age: 0, expected: AgeErr},
 	{fname: "sanket", rollnum: "t77", adress: "abcd, defgh, jiop", age: -18, expected: AgeErr},
+	{fname: "sanket", rollnum: "t78", adress: "abcd, defgh, jiop", age: 18, expected: nil},
+	{fname: "sanket22", rollnum: "t79", adress: "abcd, defgh, jiop", age: 16, expected: nil},
 }
 
+//testing validators
 func TestDataValidator(t *testing.T) {
+
 	for _, val := range testForValidator {
 		userdata := make(map[string]interface{})
 		userdata["fname"] = val.fname
 		userdata["rollnum"] = val.rollnum
 		userdata["address"] = val.adress
 		userdata["age"] = val.age
-		err := InputValidator(userdata)
+
+		err := inputValidator(userdata)
 		if err != val.expected {
 			t.Error("error in testcase #1", val.expected, err)
 		}
+	}
+}
+
+//testing add function
+func TestAdd(t *testing.T) {
+
+	for _, val := range testForValidator {
+
+		userdata := make(map[string]interface{})
+		userdata["fname"] = val.fname
+		userdata["rollnum"] = val.rollnum
+		userdata["address"] = val.adress
+		userdata["age"] = val.age
+		userdata["courses"] = constants.AllCourses[:4]
+
+		data, err := Add(userdata)
+		if err != nil {
+			if err != val.expected {
+				t.Error(1, err, data)
+			}
+		}
+
+	}
+}
+
+var testsForDisplay = []struct {
+	fname   string
+	rollnum string
+	adress  string
+	age     int
+}{
+	{fname: "sanket", rollnum: "s77", adress: "abcd, defgh, jiop", age: 17},
+	{fname: "sanket", rollnum: "s78", adress: "abcd, defgh, jiop", age: 18},
+	{fname: "sumanth", rollnum: "s79", adress: "abcd, defgh, jiop", age: 28},
+	{fname: "sanket", rollnum: "s90", adress: "abcd, defgh, jiop", age: 18},
+	{fname: "sanket22", rollnum: "s88", adress: "abcd, defgh, jiop", age: 16},
+}
+
+//check for age sorting
+func isDataAgesorted(data []user.User) bool {
+	for i := 1; i < len(data); i++ {
+		if data[i].Age > data[i].Age {
+			return false
+		}
+	}
+	return true
+}
+
+//check for rollnum sorting
+func isDataRollnumsorted(data []user.User) bool {
+	for i := 1; i < len(data); i++ {
+		if data[i].RollNo > data[i].RollNo {
+			return false
+		}
+	}
+	return true
+}
+
+//check for namewise sorting
+func isDataNamesorted(data []user.User) bool {
+	for i := 1; i < len(data); i++ {
+		if data[i].Fname > data[i].Fname {
+			return false
+		}
+	}
+	return true
+}
+
+//testing display with required sort
+func TestDisplay(t *testing.T) {
+
+	for i, val := range testsForDisplay {
+		userdata := make(map[string]interface{})
+		userdata["fname"] = val.fname
+		userdata["rollnum"] = val.rollnum
+		userdata["address"] = val.adress
+		userdata["age"] = val.age
+		userdata["courses"] = constants.AllCourses[:4]
+		data, err := Add(userdata)
+		if err != nil {
+			t.Error(2, i, err, data)
+		}
+	}
+
+	//check for age
+	mp := make(map[string]interface{})
+	mp["field"] = "age"
+	mp["order"] = 1
+	data, err := Display(mp)
+	if err != nil {
+		t.Error(err)
+	}
+	if !isDataAgesorted(data) {
+		t.Error("not sorted according to age")
+	}
+
+	//check for rollnum
+	mp["field"] = "rollnum"
+	mp["order"] = 1
+	data, err = Display(mp)
+	if err != nil {
+		t.Error(err)
+	}
+	if !isDataRollnumsorted(data) {
+		t.Error("not sorted according to rollnum")
+	}
+
+	//check for name
+	mp["field"] = "name"
+	mp["order"] = 1
+	data, err = Display(mp)
+	if err != nil {
+		t.Error(err)
+	}
+	if !isDataNamesorted(data) {
+		t.Error("not sorted according to rollnum")
+	}
+
+}
+
+//testing delete function
+func TestForDelete(t *testing.T) {
+	mp := make(map[string]interface{})
+	mp["rollnum"] = "s77"
+
+	msg, err := Delete(mp)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(msg)
+
+	msg, err = Delete(mp)
+	if err == nil {
+		t.Error("record is not deleted properly")
+	} else if err != adapters.RecordNotFound {
+		t.Error("not a proper error message", msg)
 	}
 }
