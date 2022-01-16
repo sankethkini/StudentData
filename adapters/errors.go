de094f2f1f93d4ed77ade805c90e57167cf6df74
sankethkini <kinisanketh@gmail.com>
2022-01-16 22:31:46 +0530

    removed unwanted files

--- adapters/errors.go
+++
@@ -1,7 +0,0 @@
-package adapters
-
-import "errors"
-
-var NoAdapterFound = errors.New("no matching adapters found")
-var NotARightType = errors.New("not a right type")
-var RecordNotFound = errors.New("record not found")
